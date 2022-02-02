4fc9c5e6e74de19bf44b43b5b293f9cdc18c0e74
Joaqo99 <joaqo9909@gmail.com>
2022-02-02 20:44:05 -0300

    posicionamiento de elementos

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
-
+  <div class="contenedor">
+    <Reloj />
+    <div class="menu-botones">
+      <button>Start</button>
+      <button>Reset</button>
+    </div>
+  </div>
 </template>
 
 <script>
 
+  import Reloj from "./components/Reloj.vue"
 
 export default {
   name: 'App',
-  components: {
-
-  }
+  components: { Reloj, },
+  data(){}
 }
 </script>
 
 <style>
 #app {
+  color: #aaa;
+  font-size: 8em;
+  font-family: 'Be Vietnam Pro', sans-serif;
+  background: #000;
+}
 
+.contenedor{
+  display: flex;
+  height: 100vh;
+  margin: auto 35%;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  flex-wrap: wrap;
+  text-align: center;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Reloj.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contenedor-reloj">
+        <div id="reloj">{{ display }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            display: "00:00",
+            running: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
