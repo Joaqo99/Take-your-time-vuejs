dd79480b082c9e229aad1b82cd1f4c2356beac3e
Joaqo99 <joaqo9909@gmail.com>
2022-02-02 23:02:25 -0300

    Estilos de elementos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="contenedor">
     <Reloj />
-    <div class="menu-botones">
-      <button>Start</button>
-      <button>Reset</button>
-    </div>
   </div>
 </template>
 
@@ -25,17 +21,5 @@ export default {
   font-size: 8em;
   font-family: 'Be Vietnam Pro', sans-serif;
   background: #000;
-}
-
-.contenedor{
-  display: flex;
-  height: 100vh;
-  margin: auto 35%;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-wrap: wrap;
-  text-align: center;
-  box-sizing: border-box;
 }
 </style>
--- src/components/Reloj.vue
+++ src/components/Reloj.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="contenedor-reloj">
-        <div id="reloj">{{ display }}</div>
+    <div class="contenedor">
+        <div class="contenedor-reloj">
+            <div id="reloj">{{ display }}</div>
+        </div>
+        <div class="menu-botones">
+            <button id="control" class="boton" type="button">Start</button>
+            <button id="reset" class="boton reset" type="reset">Reset</button>
+        </div>
     </div>
 </template>
 
@@ -17,4 +23,59 @@ export default {
 
 <style scoped>
 
+
+.contenedor{
+    display: flex;
+    height: 100vh;
+    margin: auto 35%;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    flex-wrap: wrap;
+    text-align: center;
+    box-sizing: border-box;
+}
+
+.contenedor-reloj{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#reloj{
+    color: #999;
+    width: 100%;
+}
+
+.menu-botones{
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    flex-wrap: nowrap;
+}
+
+.boton{
+    margin: auto .5em;
+    padding: 2px 1.5em;
+    font-size: 20px;
+    border: 2px solid #999;
+    border-radius: 20px;
+    background-color: #000;
+    color: #999;
+    cursor: pointer;
+    font-weight: 700;
+    width: 40%;
+}
+
+
+.boton:hover{
+    border-color: #999;
+    background-color: #999;
+    color: #000;
+}
+
+.reset:hover{
+    border-color: #e22;
+    background-color: #e22;
+    color: #000;
+}
 </style>
